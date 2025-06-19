ce68e95754615edf225c6cc3bb68b8487fdf3469
Alex Tiron <alexandru.bogdan.tiron10@gmail.com>
2025-06-19 21:40:14 +0300

    Update cart state management

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,16 +7,24 @@ export const CartSlice = createSlice({
    },
    reducers: {
       addItem: (state, action) => {
-         const item = action.payload;
-         const existingItem = state.items.find((i) => i.name === item.name);
+         const { name, image, cost } = action.payload;
+         const existingItem = state.items.find((item) => item.name === name);
          if (existingItem) {
-            existingItem.quantity += 1;
+            existingItem.quantity++;
          } else {
-            state.items.push({ ...item, quantity: 1 });
+            state.items.push({ name, image, cost, quantity: 1 });
          }
       },
-      removeItem: (state, action) => {},
-      updateQuantity: (state, action) => {},
+      removeItem: (state, action) => {
+         state.items = state.items.filter((item) => item.name !== action.payload);
+      },
+      updateQuantity: (state, action) => {
+         const { name, quantity } = action.payload;
+         const itemToUpdate = state.items.find((item) => item.name === name);
+         if (itemToUpdate) {
+            itemToUpdate.quantity = quantity;
+         }
+      },
    },
 });
 
