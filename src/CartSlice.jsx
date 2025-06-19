df02905b34ca7bb7acee04f80a257c556058aa02
Alex Tiron <alexandru.bogdan.tiron10@gmail.com>
2025-06-19 21:36:00 +0300

    Added Plants list + add to cart functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
+   name: "cart",
+   initialState: {
+      items: [], // Initialize items as an empty array
+   },
+   reducers: {
+      addItem: (state, action) => {
+         const item = action.payload;
+         const existingItem = state.items.find((i) => i.name === item.name);
+         if (existingItem) {
+            existingItem.quantity += 1;
+         } else {
+            state.items.push({ ...item, quantity: 1 });
+         }
+      },
+      removeItem: (state, action) => {},
+      updateQuantity: (state, action) => {},
+   },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
